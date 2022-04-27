d9d421cc4895a1b58807b3a8aadff0bb0fc2fbb7
Miguel <upn.zacatecas2@gmail.com>
2022-04-27 10:00:43 -0500

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp_packman/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace WindowsFormsApp_packman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.picturePhantomV = new System.Windows.Forms.PictureBox();
+            this.picturePhantom = new System.Windows.Forms.PictureBox();
+            this.picFrame = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picturePhantomV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePhantom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFrame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // picturePhantomV
+            // 
+            this.picturePhantomV.BackgroundImage = global::WindowsFormsApp_packman.Properties.Resources.panthomvd;
+            this.picturePhantomV.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picturePhantomV.Location = new System.Drawing.Point(200, 100);
+            this.picturePhantomV.Name = "picturePhantomV";
+            this.picturePhantomV.Size = new System.Drawing.Size(50, 46);
+            this.picturePhantomV.TabIndex = 3;
+            this.picturePhantomV.TabStop = false;
+            // 
+            // picturePhantom
+            // 
+            this.picturePhantom.BackgroundImage = global::WindowsFormsApp_packman.Properties.Resources.Phantom01;
+            this.picturePhantom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picturePhantom.Location = new System.Drawing.Point(100, 100);
+            this.picturePhantom.Name = "picturePhantom";
+            this.picturePhantom.Size = new System.Drawing.Size(50, 46);
+            this.picturePhantom.TabIndex = 2;
+            this.picturePhantom.TabStop = false;
+            // 
+            // picFrame
+            // 
+            this.picFrame.BackgroundImage = global::WindowsFormsApp_packman.Properties.Resources.pacmani01;
+            this.picFrame.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picFrame.Location = new System.Drawing.Point(83, 99);
+            this.picFrame.Name = "picFrame";
+            this.picFrame.Size = new System.Drawing.Size(58, 50);
+            this.picFrame.TabIndex = 1;
+            this.picFrame.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 20;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picturePhantomV);
+            this.Controls.Add(this.picturePhantom);
+            this.Controls.Add(this.picFrame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.picturePhantomV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePhantom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFrame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox picFrame;
+        private System.Windows.Forms.PictureBox picturePhantom;
+        private System.Windows.Forms.PictureBox picturePhantomV;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApp_packman/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_packman
+{
+    public partial class Form1 : Form
+    {
+
+        private Image[] pacmanImage = new Image[4];
+        private int currentMouthPosition = 0;
+        private int xPosition = 0;
+        private int yPosition = 0;
+        private int PxPh = 100;
+        private int PyPh = 100;
+        private int incdecx = 5;
+        private int incdecy = 5;
+        private int movpacd = 1;
+
+        // The index of the current frame.
+        private int FrameNum = 0;
+
+ 
+
+        public Form1()
+        {
+            InitializeComponent();
+            //this.KeyDown += newSystem.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+        }
+        private void picFrame_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(pacmanImage[currentMouthPosition], xPosition, yPosition, 32, 32);
+
+        }
+
+        // Load the images.
+        private void Pacman_Load(object sender, EventArgs e)
+        {
+            pacmanImage[0] = Image.FromFile("..\\..\\pacman01.gif");
+            pacmanImage[1] = Image.FromFile("..\\..\\pacman02.gif");
+            pacmanImage[2] = Image.FromFile("..\\..\\pacman03.gif");
+            pacmanImage[3] = Image.FromFile("..\\..\\pacman04.gif");
+            // Display the first frame.
+            picFrame.Image = pacmanImage[FrameNum];
+        }
+
+
+     
+
+        // Display the next image.
+        private void tmrNextFrame_Tick(object sender, EventArgs e)
+        {
+            FrameNum = ++FrameNum % pacmanImage.Length;
+            picFrame.Image = pacmanImage[FrameNum];
+        }
+
+    
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer2.Start();
+        }
+        // This is where I have tried to get the pictureBox to move
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    //picFrame.Location = new Point(10,10);
+                    picFrame.Left -= 5;
+                    picFrame.BackgroundImage =
+                              WindowsFormsApp_packman.Properties.Resources.pacmand01; 
+
+                    break;
+                case Keys.Right:
+                    picFrame.Left += 5;
+                    break;
+                case Keys.Up:
+                    picFrame.Top -= 5;
+                    break;
+                case Keys.Down:
+                    picFrame.Top += 5;
+                    break;
+            }
+        }
+
+  
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+      
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MuevePanthom();
+        }
+        void MuevePanthom()
+        {
+            if (PxPh > 600)
+                incdecx = -5; ;
+            if (PxPh < 1)
+                incdecx = 5;
+            PxPh += incdecx;
+            picturePhantom.Left += incdecx;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            MuevePanthomV();
+        }
+        void MuevePanthomV()
+        {
+            if (PyPh > 400)
+                incdecy = -5; ;
+            if (PyPh < 1)
+                incdecy = 5;
+            PyPh += incdecy;
+            picturePhantomV.Top += incdecy;
+        }
+    }
+}
